67a253253d6a1f43ced42c26877be16616e1457a
Kapil Jugnu <kr_kapil@hotmail.com>
2025-04-10 10:13:40 +0530

    validation on key stroke

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 
 export default function Login() {
   const [data, setData] = useState({email:'', password:''});
-  
+
+  const emailIsInvalid = data.email !== '' && !data.email.includes('@');  
   
   function handleSubmit(e) {
     e.preventDefault();
@@ -21,6 +22,7 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" value={data.email} onChange={(e)=>handleInputChange('email', e.target.value)} />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email</p>}</div>
         </div>
 
         <div className="control no-margin">
