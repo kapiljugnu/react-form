8560c9af97bda2ed12e7b69172b717846ebc2872
Kapil Jugnu <kr_kapil@hotmail.com>
2025-04-10 10:41:53 +0530

    validation html builtin

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -15,13 +15,13 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" required />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" required minLength={6} />
         </div>
 
         <div className="control">
@@ -30,6 +30,8 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            required
+            minLength={6}
           />
         </div>
       </div>
@@ -39,12 +41,12 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name" required />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name" required />
         </div>
       </div>
 
@@ -89,7 +91,7 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
+          <input type="checkbox" id="terms-and-conditions" name="terms" required />I
           agree to the terms and conditions
         </label>
       </div>
